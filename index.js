914edda119a2af2aaac36a75de0bfd6e744e93ee
Nathan Sainsbury <nathan.sainsbury@epas-ltd.com>
2018-04-24 15:53:01 +0100

    *

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ app.get('/', function(req,res){
 	res.send("hi");
 })
 
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 var port = process.env.PORT || 8080;
 //Endpoints
 worldPay.do(app, connection, request, postcode);
